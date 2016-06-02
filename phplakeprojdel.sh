cedfe2d2b6b63830596d7bb97d53c8c66cd57041
Washim Ahmed <washim.ahmed@gmail.com>
2016-06-02 09:39:44 -0400

    code updated

--- phplakeprojdel.sh
+++ phplakeprojdel.sh
@@ -3,4 +3,6 @@
 USERNAME=$1
 PROJECT=$2
 ENV="dev"
-rm -rf /var/www/html/$ENV-$PROJECT-$USERNAME
+CLOUD=".phplake.com"
+rm /var/www/html/ide-$USERNAME$CLOUD/workspace/$ENV-$PROJECT-$USERNAME$CLOUD
+rm -rf /var/www/html/$ENV-$PROJECT-$USERNAME$CLOUD
